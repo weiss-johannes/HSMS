47aae8491b737b7980bc35f1471ac76530237c39
Saiy0o0 <98736538+Saiy0o0@users.noreply.github.com>
2023-02-08 09:51:18 +0100

    Update w_erstellen.php
    ALLE TABELLEN VORRAUSSICHTLICH ERSTELLT (NICHT GETESTET)

--- w_erstellen.php
+++ w_erstellen.php
@@ -22,7 +22,7 @@
     $fall = "DROP DATABASE IF EXISTS weihnachten";
     mysqli_query($link,$fall);
 
-    $erst = "CREATE DATABASE weihnachten IF NOT EXISTS";
+    $erstData = "CREATE DATABASE weihnachten IF NOT EXISTS";
     if(mysqli_query($link,$erst))
     {
         echo "<h2>(•_•) Erstellt (•_•)</h2>";
@@ -33,6 +33,56 @@
     {
         echo "<h2>ಠ_ಠVerbindung zur Datenbank erstelltಠ_ಠ</h2>";
     }
+
+    $erstEngel='CREATE TABLE IF NOT EXISTS engel
+        (
+            e_name varchar(20) NOT NULL,
+            funktion varchar(10) NOT NULL,
+            dienstgrad smallint(1) DEFAULT 1,
+            aufgabe varchar(500),
+            PRIMARY KEY (e_name)            
+        )';
+
+    if(mysqli_query($link,$erstEngel))
+    {
+        echo "<h2>(❁´◡`❁)ENGEL SIND ZU UNS GEKOMMEN(❁´◡`❁)</h2>";
+    }
+
+    $erstSpiel='CREATE TABLE IF NOT EXISTS spielzeug
+        (
+            id varchar(8) NOT NULL,
+            bez varchar(200) NOT NULL,
+            wert smallint(5),
+            laenge smallint(3),
+            breite smallint(3),
+            hoehe smallint(3),
+            m_alter smallint(2),
+            anzahl int(10),            
+            PRIMARY KEY (id)            
+        )';
+
+    if(mysqli_query($link,$erstSpiel))
+    {
+        echo "<h2>(⊙_⊙;) EINFACH SPIELZEUG-MACHER (⊙_⊙;)</h2>";
+    }
+
+    $erstKind='CREATE TABLE IF NOT EXISTS kinder
+        (
+            knr int(10) AUTO INCREMENT
+            k_name varchar(30) NOT NULL,
+            vorname varchar(30) NOT NULL,
+            wohnort varchar(50) NOT NULL,
+            geschlecht enum("m", "w"),
+            gebdat DATE,
+            charakter smallint(1),
+            PRIMARY KEY (knr)            
+        )';
+
+    if(mysqli_query($link,$erstKind))
+    {
+        echo "<h2>¯\_(ツ)_/¯MEINE SIND SIE NICHT¯\_(ツ)_/¯</h2>";
+    }
+
 
 
 
