a2b87ebc2c2670cedbb0e4bf0ee4a30a7810b17a
Alex Glaser <119404263+glaser-alex@users.noreply.github.com>
2023-02-13 10:48:15 +0100

    index.php

--- index.php
+++ index.php
@@ -11,17 +11,14 @@
   <body style="cursor: url(./img/cursor/Christian_cross.svg), auto;">
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #0431f9;">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img src="./img/logo/original/logo.png" alt="Logo" width="100"  class="d-inline-block align-text-top">
+          <a class="navbar-brand" href="index.php?action=home">
+            <img src="./img/logo/original/logo.png" alt="Logo" width="100" class="d-inline-block align-text-top">
             </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.php?action=home">Home</a>
-              </li>
               <li class="nav-item">
                 <a class="nav-link" href="index.php?action=engel">Engel</a>
               </li>
@@ -32,9 +29,7 @@
                 <a class="nav-link" href="index.php?action=eintragen">Eintragen</a>
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Kinder
-                </a>
+                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Kinder</a>
                 <ul class="dropdown-menu">
                   <li><a class="dropdown-item" href="index.php?action=uebersicht">Übersicht</a></li>
                   <li><hr class="dropdown-divider"></li>
@@ -42,14 +37,12 @@
                 </ul>
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-danger" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Einstellungen
-                </a>
+                <a class="nav-link dropdown-toggle text-danger" role="button" data-bs-toggle="dropdown" aria-expanded="false">Einstellungen</a>
                 <ul class="dropdown-menu">
                   <li><a class="dropdown-item text-danger" href="index.php?action=erstellen">Datenbank erstellen</a></li>
                   <li><hr class="dropdown-divider"></li>
                   <li><a class="dropdown-item" href="index.php?action=eintragen">Daten eintragen</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  <li><a class="dropdown-item" href="index.php?action=something">Something else here</a></li>
                 </ul>
               </li>
               <li class="nav-item">
@@ -86,6 +79,18 @@ if ($action == 'aenderungen') {
 
 if ($action == 'erstellen') {
   require("./w_erstellen.php");
+}
+
+if ($action == 'something') {
+  echo "<style>
+          body {
+            animation: background-color-change 0.5s infinite;
+          }
+
+          @keyframes background-color-change {
+            50% { background-color: red; }
+          }
+        </style>";
 }
 ?>
   </body>
