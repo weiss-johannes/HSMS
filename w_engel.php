6eff519c19266c2e65d18c642b6ede5526e466b3
Kurzschluss01 <email@weiss-johannes.de>
2023-02-13 10:56:18 +0100

    Update w_engel.php

--- w_engel.php
+++ w_engel.php
@@ -10,8 +10,24 @@
 <body>
 <?php
 
+require("./db_in_pruefung.php");
+
+$query="SELECT * FROM engel WHERE dienstgrad > 5";
+
 mysqli_query($link, $query);
 
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+      echo "Name: " . $row["e_name"]. " - Funktion: " . $row["funktion"]. " dienstgrad " . $row["dienstgrad"]. 
+      " aufgaben " . $row["aufgaben"]. "<br>";
+    }
+  } else {
+    echo "0 ergebnisse";
+  }
+
+$query = "ALTER TABLE engel ADD urlaubstage int; 
+
+mysqli_query($link, $query);
 
 
 ?> 
